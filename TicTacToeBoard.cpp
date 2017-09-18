de63a7e2e897e1b1c7b5098aa59759aa9d20714a
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-18 18:49:12 +0000

    creation of code for checking for a TicTacToe Winner

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -51,5 +51,22 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
+  // Check Horizontals
+  for (int i = 0; i < 3; i++){
+    if (board[i][0] == board[i][1] == board[i][2]) {
+      return board[i][0];
+    }
+  }
+  // Check Verticals
+  for (int i = 0; i < 3; i++){
+    if (board[0][i] == board[1][i] == board[2][i]) {
+      return board[0][i];
+    }
+  }
+  // Check Diagonals
+  if (board[0][0] == board[1][1] == board [2][2] || 
+      board[0][2] == board[1][1] == board[2][0]) { 
+    return true;
+  }
   return Invalid;
 }
