9c8cdb8e7c33dc6786b33e6dce18a1be6a890c9e
Justin K Guillermo <jguillermo@mail.csuchico.edu>
2017-09-18 23:19:51 -0700

    Changed functions

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -1,55 +1,92 @@
 #include "TicTacToeBoard.h"
 /**
- * Class for representing a 3x3 Tic-Tac-Toe game board, using the Piece enum
- * to represent the spaces on the board.
+* Class for representing a 3x3 Tic-Tac-Toe game board, using the Piece enum
+* to represent the spaces on the board.
 **/
 
 //Constructor sets an empty board and specifies it is X's turn first
 TicTacToeBoard::TicTacToeBoard()
 {
-  turn = X;
-  for(int i=0; i<BOARDSIZE; i++)
-    for(int j=0; j<BOARDSIZE; j++)
-      board[i][j] = Blank;
+	turn = X;
+	for (int i = 0; i<BOARDSIZE; i++)
+		for (int j = 0; j<BOARDSIZE; j++)
+			board[i][j] = Blank;
 }
 
 /**
- * Switches turn member variable to represent whether it's X's or O's turn
- * and returns whose turn it is
+* Switches turn member variable to represent whether it's X's or O's turn
+* and returns whose turn it is
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+	if (turn == X) {
+		turn = O; return O;
+	}
+	else {
+		turn = X; return X;
+	}
 }
-
 /**
- * Places the piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. placePiece does 
- * NOT allow to place a piece in a location where there is already a piece.
- * In that case, placePiece just returns what is already at that location. 
- * Out of bounds coordinates return the Piece Invalid value. When the game
- * is over, no more pieces can be placed so attempting to place a piece
- * should neither change the board nor change whose turn it is.
-**/ 
+* Places the piece of the current turn on the board, returns what
+* piece is placed, and toggles which Piece's turn it is. placePiece does
+* NOT allow to place a piece in a location where there is already a piece.
+* In that case, placePiece just returns what is already at that location.
+* Out of bounds coordinates return the Piece Invalid value. When the game
+* is over, no more pieces can be placed so attempting to place a piece
+* should neither change the board nor change whose turn it is.
+**/
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+	if (row > 0 || row < 2 || column > 0 || column < 2) {
+		return Invalid;
+	}
+	else if (board[row][column] == ' ') {
+		if (turn == X) {
+			board[row][column] = X; turn = O; return X;
+		}
+		else {
+			board[row][column] = O; turn = X; return O;
+		}
+	}
+	else {
+		return board[row][column];
+	}
 }
 
 /**
- * Returns what piece is at the provided coordinates, or Blank if there
- * are no pieces there, or Invalid if the coordinates are out of bounds
+* Returns what piece is at the provided coordinates, or Blank if there
+* are no pieces there, or Invalid if the coordinates are out of bounds
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+	if (row < 1 || row > 3 || column < 1 || column > 3) {
+		return Invalid;
+	}
+	else if (board[row][column] == ' ') return Blank;
+	else if (board[row][column] == 'X') return X;
+	else { return O; }
 }
 
 /**
- * Returns which Piece has won, if there is a winner, Invalid if the game
- * is not over, or Blank if the board is filled and no one has won.
+* Returns which Piece has won, if there is a winner, Invalid if the game
+* is not over, or Blank if the board is filled and no one has won.
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (board[i][j] == Blank || board[j][i] == Blank) return Blank;
+		}
+	}
+
+	if ((board[0][0] == board[0][1] == board[0][2]) || (board[0][0] == board[1][0] == board[2][0]) || (board[0][0] == board[1][1] == board[2][2]) : ) {
+		return board[0][0];
+	}
+	else if ((board[0][1] == board[1][1] == board[2][1]) || board[1][0] == board[1][1] == board[1][2]) {
+		return board[1][1];
+	}
+	else if (board[2][0] == board[2][1] == board[2][2] || board[0][2] == board[1][2] == board[2][2]) {
+		return board[2][2];
+	}
+	return Invalid;
 }
