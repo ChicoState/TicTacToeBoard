6dc6bd42de91cd750601aac907f56fb39556a04c
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-20 21:37:18 +0000

    Added validation tests

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -20,3 +20,135 @@ TEST(TicTacToeBoardTest, sanityCheck)
 	ASSERT_TRUE(true);
 }
 */
+
+TEST(TicTacToeBoardTest, toggleTurn) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(O,local_board.toggleTurn());
+}
+
+TEST(TicTacToeBoardTest, toggleTurnTwice) {
+	TicTacToeBoard local_board;	
+	local_board.toggleTurn();
+	ASSERT_EQ(X,local_board.toggleTurn());
+}
+
+
+TEST(TicTacToeBoardTest, getPiece) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Blank,local_board.getPiece(0,0));
+}
+
+TEST(TicTacToeBoardTest, getPieceOOBNegZero) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Invalid,local_board.getPiece(-1,0));
+}
+
+TEST(TicTacToeBoardTest, getPieceOOBZeroNeg) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Invalid,local_board.getPiece(0,-1));
+}
+
+TEST(TicTacToeBoardTest, getPieceOOBNegNeg) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Invalid,local_board.getPiece(-1,-1));
+}
+
+
+TEST(TicTacToeBoardTest, getPieceOOBPosZero) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Invalid,local_board.getPiece(3,0));
+}
+
+TEST(TicTacToeBoardTest, getPieceOOBZeroPos) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Invalid,local_board.getPiece(0,3));
+}
+
+TEST(TicTacToeBoardTest, getPieceOOBPosPos) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Invalid,local_board.getPiece(3,3));
+}
+
+TEST(TicTacToeBoardTest, placePieceX) {
+	TicTacToeBoard local_board;	
+	ASSERT_EQ(Blank,local_board.getPiece(0,0));
+	local_board.placePiece(0,0);
+	ASSERT_EQ(X,local_board.getPiece(0,0));
+}
+
+TEST(TicTacToeBoardTest, placePieceO) {
+	TicTacToeBoard local_board;	
+	local_board.toggleTurn();
+	ASSERT_EQ(Blank,local_board.getPiece(0,0));
+	local_board.placePiece(0,0);
+	ASSERT_EQ(O,local_board.getPiece(0,0));
+}
+
+TEST(TicTacToeBoardTest, HorizontalWinX) {
+	TicTacToeBoard local_board;	
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(i,0);
+	}
+	ASSERT_EQ(local_board.getPiece(0,0),local_board.getWinner());
+}
+
+TEST(TicTacToeBoardTest, HorizontalWinO) {
+	TicTacToeBoard local_board;	
+	local_board.toggleTurn();
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(i,0);
+	}
+	ASSERT_EQ(local_board.getPiece(0,0),local_board.getWinner());
+}
+
+TEST(TicTacToeBoardTest, VerticalWinX) {
+	TicTacToeBoard local_board;	
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(0,i);
+	}
+	ASSERT_EQ(local_board.getPiece(0,0),local_board.getWinner());
+}
+
+TEST(TicTacToeBoardTest, VerticalWinO) {
+	TicTacToeBoard local_board;	
+	local_board.toggleTurn();
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(0,i);
+	}
+	ASSERT_EQ(local_board.getPiece(0,0),local_board.getWinner());
+	
+}
+
+TEST(TicTacToeBoardTest, DiagonalWinX1) {
+	TicTacToeBoard local_board;	
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(i,i);
+	}
+	ASSERT_EQ(local_board.getPiece(1,1),local_board.getWinner());
+}
+
+TEST(TicTacToeBoardTest, DiagonalWinX2) {
+	TicTacToeBoard local_board;	
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(i,BOARDSIZE-i-1);
+	}
+	ASSERT_EQ(local_board.getPiece(1,1),local_board.getWinner());
+}
+
+TEST(TicTacToeBoardTest, DiagonalWinO1) {
+	TicTacToeBoard local_board;	
+	local_board.toggleTurn();
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(i,i);
+	}
+	ASSERT_EQ(local_board.getPiece(1,1),local_board.getWinner());
+}
+
+TEST(TicTacToeBoardTest, DiagonalWinO2) {
+	TicTacToeBoard local_board;	
+	local_board.toggleTurn();
+	for(int i = 0; i < BOARDSIZE; i++) {
+		local_board.placePiece(i,BOARDSIZE-i-1);
+	}
+	ASSERT_EQ(local_board.getPiece(1,1),local_board.getWinner());
+}
