a71877c5fe20060e4d732688e4755240e1958e59
JonRudy <kbuffardi@csuchico.edu>
2018-04-23 08:56:11 +0000

    Adding implementation and Testing

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -20,3 +20,62 @@ TEST(TicTacToeBoardTest, sanityCheck)
 	ASSERT_TRUE(true);
 }
 */
+
+TEST(TicTacToeBoardTest, sanityCheck)
+{
+	TicTacToeBoard board;
+	ASSERT_TRUE(true);
+}
+
+TEST(TicTacToeBoardTest, testPieceX)
+{
+	TicTacToeBoard board;
+	board.toggleTurn();
+	ASSERT_TRUE(board.toggleTurn() == 'X');
+}
+
+TEST(TicTacToeBoardTest, testPieceO)
+{
+	TicTacToeBoard board;
+	ASSERT_TRUE(board.toggleTurn() == 'O');
+}
+
+TEST(TicTacToeBoardTest, invalidLocationRow)
+{
+	TicTacToeBoard board;
+	ASSERT_FALSE(board.placePiece(3,2) == 'X');
+}
+
+TEST(TicTacToeBoardTest, invalidLocationColumn)
+{
+	TicTacToeBoard board;
+	ASSERT_FALSE(board.placePiece(2,3) == 'X');
+}
+
+TEST(TicTacToeBoardTest, validLocationRow)
+{
+	TicTacToeBoard board;
+	ASSERT_TRUE(board.placePiece(2,2) == 'X');
+}
+
+TEST(TicTacToeBoardTest, validLocationColumn)
+{
+	TicTacToeBoard board;
+	ASSERT_TRUE(board.placePiece(0,1) == 'X');
+}
+
+TEST(TicTacToeBoardTest, existingPeice)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,1);
+	ASSERT_FALSE(board.placePiece(0,1) == 'X');
+}
+
+TEST(TicTacToeBoardTest, freshGame)
+{
+	TicTacToeBoard board;
+	ASSERT_TRUE(board.getPiece(0,0) == Blank);
+}
+
+
+
